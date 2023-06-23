15aa43f170768f16af87f6cf8622fc78febd6e7b
Terry1852 <136498436+Terry1852@users.noreply.github.com>
2023-06-23 11:02:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// ReactDom.render(<h1>Hello, everyone!</h1>, document.querySelector("root"));
+
+// let newH1 = document.createElement("h1")
+// newH1.textContent = "Oh, my lord!"
+// newH1.className = "header"
+// document.getElementById("root").appendChild(h1)
+
+const navbar = (
+    <nav>
+        <h1>BellerLooker</h1>
+        <ul><li>Pricing</li><li>About</li><li>Contact</li></ul>
+    </nav>
+)
